a1889f7edb47a6980d9e10dc78b9d99302fdef56
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-07-19 12:02:06 +0700

    lab3

---
+++ Labs/Lab3F/Program.cs
@@ -0,0 +1,33 @@
+﻿char A = 'A';
+char Z = 'Z';
+int total_vowel = 0, total_consonant = 0;
+
+for (int i= (int)A; i<= (int)Z; i++)
+{
+    char c = (char)i;
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'U' || c == 'O') {
+        Console.WriteLine(c + " : " + i + " - Vowel");
+        total_vowel++;
+    }
+    else
+    {
+        Console.WriteLine(c + " : " + i + " - Consonant");
+        total_consonant++;
+    }
+}
+Console.WriteLine("Total vowel: " + total_vowel);
+Console.WriteLine("Total consonant: " + total_consonant);
+
+
+char a = 'a';
+char z = 'z';
+
+for (int i = (int)a; i <= (int)z; i++)
+{
+    char c = (char)i;
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'u' || c == 'o')
+        Console.WriteLine(c + " : " + i + " - Vowel");
+    else
+        Console.WriteLine(c + " : " + i + " - Consonant");
+}
+
---
+++ Labs/Lab3G/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter first name: ");
+String firstName = Console.ReadLine();
+Console.Write("Enter last name: ");
+String lastName = Console.ReadLine();
+Console.Write("Enter delimiter: ");
+String delimiter = Console.ReadLine();
+Console.WriteLine(firstName + delimiter + lastName);
