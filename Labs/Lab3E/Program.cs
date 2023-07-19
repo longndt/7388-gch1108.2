79aeecad1da1467124d84ebd02fa390bbed07bba
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-07-19 11:11:10 +0700

    lab03

---
+++ Labs/Lab3E/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * String: many characters, double quote
+ * Character: only 1 character, single quote
+ */
+
+String greenwich = "Greenwich Vietnam";
+char c = '$';
+Console.WriteLine(greenwich);
+Console.WriteLine(c);
+
+Console.Write("Enter your gender (M/F): ");
+char gender = char.Parse(Console.ReadLine());
+gender = char.ToUpper(gender);
+if (gender == 'M')
+{
+    Console.WriteLine("You are male !");
+} else if (gender == 'F')
+{
+    Console.WriteLine("You are female !");
+} else
+{
+    Console.WriteLine("Invalid gender !");
+}
