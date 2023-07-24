4709868c585e9e2b70e91ee6ad6dab95d3d95711
= <=>
2023-07-24 11:20:01 +0700

    lab4

---
+++ Labs/Lab4E/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] n = { 12, 23, 34, 45, 56 };
+String s = String.Join(" - ", n);
+Console.WriteLine(s);
+String[] s1 = { "Hanoi", "is", "the", "capital", "of", "Vietnam" };
+String s2 = String.Join(" ", s1);
+Console.WriteLine(s2);
+
+String s3 = "G r e e n w i c h  V i e t n a m";
+String[] s4 = s3.Split();
+String s5 = String.Join(" ",s4);
+Console.WriteLine(s5);
+/* for (int i = s4.Length - 1; i >= 0; i--)
+    Console.Write(s4[i]);
+*/
---
+++ Labs/Lab4F/Program.cs
@@ -0,0 +1,47 @@
+﻿List<int> numbers = new List<int>();
+numbers.Add(2023);
+numbers.Add(2040);
+numbers.Add(2060);
+numbers.Add(2080);
+
+for (int i=0; i<numbers.Count; i++)
+    Console.WriteLine(numbers[i]);
+
+foreach (var n in numbers)
+    Console.WriteLine(n);
+
+List<String> students = new List<String>();
+students.Add("Minh");
+students.Add("Tuan");
+students.Add("Hoang");
+students.Add("Hung");
+students.Add("Hoang");
+students.Add("Hung");
+students.Add("Hoang");
+students.Add("Hung");
+Console.Write("Enter student name: ");
+String name = Console.ReadLine();
+if (students.Contains(name))
+    Console.WriteLine("True");
+else
+    Console.WriteLine("False");
+
+Console.WriteLine("Total students: " + students.Count);
+students.Remove(students[1]);  //remove 2nd student ("Tuan")
+students.RemoveAt(4);          //remove 5th student (index = 4)
+Console.WriteLine("Total students: " + students.Count);
+students.Clear();
+Console.WriteLine("Total students: " + students.Count);
+
+List<int> nums = new List<int> {10, 20, 30, 40, 50};
+nums.Add(60);
+nums.Insert(3, 70);
+foreach (int n in nums)  
+    Console.WriteLine(n);
+Console.WriteLine("------------");
+nums.Sort();  //sort by ascending order (small to big)
+foreach (int n in nums)
+    Console.WriteLine(n);
+Console.WriteLine("Max: " + nums.Max());
+Console.WriteLine("Min: " + nums.Min());
+
