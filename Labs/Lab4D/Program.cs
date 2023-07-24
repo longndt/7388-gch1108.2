4b93de790f25e06ac41d815ef5c09f682bdda876
= <=>
2023-07-24 10:30:40 +0700

    lab4

---
+++ Labs/Lab4D/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter total elements in array: ");
+int total = int.Parse(Console.ReadLine());
+int[] numbers = new int[total]; 
+for (int i = 0; i < total; i++)
+{
+    Console.Write("Enter number " + (i+1) + " : ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+for (int i = total -1; i >= 0; i--)
+{
+    Console.Write(numbers[i] + " ");
+}
