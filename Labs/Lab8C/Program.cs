f837674cb7c5dee79921b55f9dcd9fce6e7a1a11
= <=>
2023-08-07 11:01:45 +0700

    lab8

---
+++ Labs/Lab8C/Program.cs
@@ -0,0 +1,31 @@
+﻿//find and remove word from string
+using System;
+
+Console.Write("Enter a text: ");
+string t = Console.ReadLine();
+Console.Write("Enter a word: ");
+string w = Console.ReadLine();
+//tìm word 'w' trong text 't' : -1 (not found), !-1 (found, return first index);
+int index = t.IndexOf(w);
+int total = 0;
+Console.WriteLine("Initial Length: " + t.Length);
+//chạy vòng lặp while để tìm và xóa tất cả các word trong string
+while (index != -1)
+{
+    total++;
+    t = t.Remove(index, w.Length); //xóa word khỏi text
+    index = t.IndexOf(w); //reset lại index của word
+}
+Console.WriteLine("Current Length: " + t.Length);
+Console.WriteLine("Total word found: " + total);    
+Console.WriteLine("Text after removed word: " + t);
+
+//find word from string
+Console.Write("Enter a text: ");
+string text = Console.ReadLine();
+Console.Write("Enter a word to search: ");
+string word = Console.ReadLine();
+if (text.Contains(word))
+    Console.Write("Word found !");
+else
+    Console.Write("Word not found !");
---
+++ Labs/Lab8D/Program.cs
@@ -0,0 +1,46 @@
+﻿//Email format: name@domain
+
+using System.ComponentModel.DataAnnotations;
+
+string email;
+int total;
+int length;
+bool check1 = false;
+bool check2 = false;
+
+//1. validate email format: email must contain only 1 '@' symbol (not at 1st or last position)
+//and must have '.' symbol
+do
+{
+    Console.Write("Enter your email: ");
+    email = Console.ReadLine();
+    char[] emails = email.ToCharArray();
+    length = emails.Length;
+    total = 0;
+    for (int i=0; i<length; i++) 
+    {
+        if (emails[i] == '@')
+        {
+            check1 = false;
+            if (i == 0 || i == length - 1)
+            {
+                break;
+            }     
+            else
+            {
+                check1 = true;
+                total++;
+            }
+        } 
+        if (emails[i] == '.')
+        {
+            check2 = true;
+        }
+    }
+} while (!email.Contains("@") || total > 1 || check1 == false || check2 == false || length < 5);
+
+
+//2. split email components by '@' symbol
+string[] components = email.Split("@");
+Console.WriteLine("Email name: " + components[0]);
+Console.WriteLine("Email domain: " + components[1]);
